a97ae243b756280ed826e2ff3a3aadc2ba53cb47
Schulz Hauptmann <xaclafun1991@gmail.com>
2023-10-19 13:40:36 +0300

    dsds

--- manage.py
+++ manage.py
@@ -13,10 +13,9 @@ def main():
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "forget to activate a virtual environmenjt?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 
 if __name__ == '__main__':
