054750a00de5bb0d7799ae6847e2ed7406484c75
Abusik019 <abusroyale@gmail.com>
2023-11-03 23:52:29 +0300

    a

--- static/src/scripts/index.js
+++ static/src/scripts/index.js
@@ -1,7 +1,93 @@
 import { updateSlider } from "../utils/slider.js";
 import adaptiveSlider from "../utils/adaptive-slider.js";
 
-// registration();
+
+
+///////////////////////////////////////////////////////////////////////////
+
+const linksDropdown = document.getElementById('drop_down_link_list'),
+      btnDropdown = document.getElementById('btn_important_links'),
+      addLink = document.getElementById('add_link'),
+      btnAddLink = document.getElementById('btn_add_link'),
+      publishLink = document.getElementById('publish_link'),
+      linkNameInput = document.getElementById('add_link_name'),
+      linkUrlInput = document.getElementById('add_link_url');
+
+
+
+const allLinks = [];
+
+
+document.addEventListener('click', (e) => {
+    const withinBoundaries = e.target.closest('#drop_down_link_list') || e.target.id === 'btn_important_links';
+    const withinBoundaries2 = e.target.closest('#add_link') || e.target.id === 'btn_add_link';
+
+    if (!withinBoundaries && !withinBoundaries2) {
+        linksDropdown.classList.remove('active');
+        addLink.classList.remove('active');
+    }
+});
+
+document.addEventListener('keydown', (e) => {
+	if( e.keyCode == 27 ){ // код клавиши Escape, но можно использовать e.key
+		linksDropdown.classList.remove('active');
+        addLink.classList.remove('active');
+	}
+});
+
+btnAddLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    addLink.classList.toggle('active');
+})
+
+btnDropdown.addEventListener('click', (event) => {
+
+
+    event.preventDefault();
+    linksDropdown.classList.toggle('active');
+})
+
+publishLink.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const linkName = linkNameInput.value;
+    const linkUrl = linkUrlInput.value;
+
+
+    if (linkName && linkUrl) {
+        allLinks.push({ name: linkName, url: linkUrl });
+
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.setAttribute('href', linkUrl);
+        a.innerHTML = linkName;
+        li.prepend(a)
+        linksDropdown.append(li);
+
+        linkNameInput.value = '';
+        linkUrlInput.value = '';
+
+        updateLastListItemStyle()
+    } else {
+        alert('Заполните все поля перед публикацией.');
+    }
+})
+
+function updateLastListItemStyle() {
+    const allListItems = document.querySelectorAll('.drop_down_link_list li');
+
+    if (allListItems.length > 0) {
+        // Устанавливаем стиль для всех li, кроме последнего
+        for (let i = 0; i < allListItems.length - 1; i++) {
+            allListItems[i].style.borderBottom = '1px solid black';
+        }
+
+        // Убираем бордер у последнего li
+        allListItems[allListItems.length - 1].style.border = 'none';
+    }
+    return
+}
+
 
 //BURGER MENU////////////////////////////////////////////////////////////////////
 
@@ -28,11 +114,6 @@ isTeacherCheckbox.addEventListener('change', function() {
     } else {
         loginLastName.innerText = 'Фамилия';
         loginPassword.innerText = 'Номер зачетной книжки'
-    }
-    if (isTeacherCheckbox.checked) {
-        login_password.innerText = 'Пароль';
-    } else {
-        login_password.innerText = 'Номер зачетной книжки';
     }
 });
 
@@ -86,6 +167,6 @@ if (window.innerWidth < 767) {
     updateSlider(slides, currentIndex);
 }
 
-//  ////////////////////////////////////////////////////////////////////
+ ////////////////////////////////////////////////////////////////////
 
 
